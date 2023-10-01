e6cd60a42bcd5fbee99831746f07810901660b4f
silvianaim02 <silvianaim68@gmail.com>
2023-10-01 12:05:48 +0700

    method routing 1 method

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const init = async () => {
     host: 'localhost',
   });
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!';
+    },
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
