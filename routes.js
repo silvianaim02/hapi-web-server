cf772496230a709630570c64968c6b4600deeed8
silvianaim02 <silvianaim68@gmail.com>
2023-10-01 13:58:47 +0700

    bodi or payload request

--- routes.js
+++ routes.js
@@ -58,6 +58,14 @@ const routes = [
     },
   },
   {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}!`;
+    },
+  },
+  {
     method: '*',
     path: '/{any*}',
     handler: (request, h) => {
